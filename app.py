f85c7843a875629fd922bb9b9f70d8093dcd2a47
juli-vera <julietavera287@gmail.com>
2026-05-08 18:17:44 -0300

    Actualizo proyecto

--- app.py
+++ app.py
@@ -1,25 +1,19 @@
-# Importo Flask para crear la aplicacion, jsonify devuelve respuestas en formato JSON, y request lee datos que envia el cliente.
 from flask import Flask, jsonify, request
 from flasgger import Swagger
-from cart import CartManager  # Importo clase que maneja logica del carrito
-# Importo clase que maneja logica de los productos
+from cart import CartManager
 from products import ProductRepository
 
-"""Creo la aplicacion Flask. __name__ indica a Flask donde buscar archivo"""
 app = Flask(__name__)
 Swagger(app)
 
-"""Creo instancia del repositorio de productos y otra de carrito para usarlas en endpoints"""
 product_repo = ProductRepository()
 cart_manager = CartManager()
 
 # PRODUCTOS
 
-# Defino endpoint GET en ruta /products para listar los productos
-
 
 @app.route("/products", methods=["GET"])
-def list_products():  # Defino la función que se ejecuta cuando se llama ese endpoint
+def list_products():
     """
     Listar todos los productos
     ---
@@ -28,17 +22,14 @@ def list_products():  # Defino la función que se ejecuta cuando se llama ese en
           description: Lista de productos
     """
 
-    # Obtengo todos los productos desde el repositorio
     products = product_repo.get_all()
-    # Devuelvo los productos en formato JSON con código 200, que indica éxito
+
     return jsonify(products), 200
 
 
 # CARRITO
 
-# Endpoint para ver el contenido del carrito
 @app.route("/cart", methods=["GET"])
-# Esta función se ejecuta cuando se consulta ese endpoint
 def get_cart():
     """
     Ver carrito
@@ -48,8 +39,8 @@ def get_cart():
         description: Productos del carrito
     """
 
-    items = cart_manager.get_items()  # Obtengo los productos que están en el carrito
-    result = []  # Creo una lista vacía donde voy a construir la respuesta final
+    items = cart_manager.get_items()
+    result = []
 
     for item in items:
         product = product_repo.get_by_id(item["product_id"])
@@ -62,10 +53,9 @@ def get_cart():
                 "quantity": item["quantity"]
             })
 
-    return jsonify(result), 200  # Devuelvo productos en formato json
+    return jsonify(result), 200
 
 
-# Endpoint para agregar productos al carrito
 @app.route("/cart/items", methods=["POST"])
 def add_to_cart():
     """
@@ -88,29 +78,23 @@ def add_to_cart():
       404:
         description: Producto no encontrado
     """
-    data = request.get_json()  # Obtengo los datos enviados por el cliente en formato JSON
+    data = request.get_json()
     if not data or "product_id" not in data:
         return jsonify({"error": "Se requiere product_id"}), 400
 
-    # Busco el producto en el repositorio
     product = product_repo.get_by_id(data["product_id"])
-    if not product:  # Si el producto no existe, devuelvo error 404
+    if not product:
         return jsonify({"error: Producto no encontrado"}), 404
 
-    # Obtengo la cantidad, y si no viene, uso 1 por defecto
     quantity = data.get("quantity", 1)
 
-    # Valido la cantidad
     if quantity < 1:
         return jsonify({"error": "La cantidad debe ser un entero positivo"}), 400
 
-    # Agrego el producto al carrito con la cantidad indicada
     cart_manager.add_item(product["id"], quantity)
-    # Devuelvo mensaje de exito
+
     return jsonify({
         "message": "Producto agregado al carrito"}), 200
-
-# Endpoint para eliminar un producto del carrito por su ID
 
 
 @app.route("/cart/items/<int:product_id>", methods=["DELETE"])
@@ -130,17 +114,13 @@ def remove_from_cart(product_id):
       404:
         description: Producto no encontrado
     """
-    # Intento eliminar el producto del carrito
     removed = cart_manager.remove_item(product_id)
-    # Si no estaba en el carrito, devuelvo error
+
     if not removed:
         return jsonify({"error": "Producto no encontrado en el carrito"}), 404
-    # Si se eliminó correctamente, devuelvo éxito
     return jsonify({
         "message": "Producto eliminado del carrito"
     }), 200
-
-# Endpoint calcula el total del carrito
 
 
 @app.route("/cart/total", methods=["GET"])
@@ -152,7 +132,6 @@ def get_total():
       200:
         description: Total del carrito
     """
-    # Uso los precios del repo para calcular el total
     total = cart_manager.calculate_total(product_repo)
     return jsonify({"Total": total}), 200
 
@@ -168,7 +147,3 @@ def clear_cart():
     """
     cart_manager.clear()
     return jsonify({"message": "Carrito vaciado exitosamente"}), 200
-
-
-# if __name__ == "__main__":
-#    app.run(debug=True)

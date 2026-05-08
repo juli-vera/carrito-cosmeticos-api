0274139e315505c6e76b7ac666044a09288cdf5c
juli-vera <167469338+juli-vera@users.noreply.github.com>
2026-05-08 16:34:32 -0300

    Update products.py

--- products.py
+++ products.py
@@ -4,14 +4,14 @@ class ProductRepository:
 
     def __init__(self):
         self._products = {
-            1: {"id": 1, "name": "Mascarilla",       "price": 3800, "stock": 10},
-            2: {"id": 2, "name": "Crema hidratante",    "price": 4810,   "stock": 50},
-            3: {"id": 3, "name": "Contorno de ojos",     "price": 9300,   "stock": 25},
-            4: {"id": 4, "name": "Base",      "price": 10000,  "stock": 8},
-            5: {"id": 5, "name": "Corrector", "price": 8900,  "stock": 30},
-            6: {"id": 6, "name": "Sombra de ojos",      "price": 2600,   "stock": 20},
-            7: {"id": 7, "name": "Delineador",     "price": 1800,   "stock": 40},
-            8: {"id": 8, "name": "Labial",      "price": 5900,  "stock": 15},
+            1: {"id": 1, "name": "Mascarilla",       "price": 3800},
+            2: {"id": 2, "name": "Crema hidratante",    "price": 4810},
+            3: {"id": 3, "name": "Contorno de ojos",     "price": 9300},
+            4: {"id": 4, "name": "Base",      "price": 10000},
+            5: {"id": 5, "name": "Corrector", "price": 8900},
+            6: {"id": 6, "name": "Sombra de ojos",      "price": 2600},
+            7: {"id": 7, "name": "Delineador",     "price": 1800},
+            8: {"id": 8, "name": "Labial",      "price": 5900},
         }
 
     def get_all(self):
